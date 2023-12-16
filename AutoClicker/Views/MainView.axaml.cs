f0087036c8efbbf063258e73697a9d401e24ed1b
J4nis05 <janis.roelli@gmail.com>
2023-12-16 19:00:23 +0100

    Avalonia Tutorial UwU

--- AutoClicker/Views/MainView.axaml.cs
+++ AutoClicker/Views/MainView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using Avalonia.Controls.Primitives;
+using ReactiveUI;
 
 namespace AutoClicker.Views;
 
@@ -7,5 +11,19 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+    
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        if (double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
     }
 }
